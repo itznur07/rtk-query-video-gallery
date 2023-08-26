79950a71a64feaa8372aee198db049a3f4da9a13
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-26 10:50:10 +0600

    Videos & Api Features Completed!

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,35 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
+  const { data, isLoading, isError } = useGetVideosQuery();
+
+  /** <!-- content render --> */
+  function contentRender() {
+    let content = null;
+    if (isLoading && !isError)
+      content = (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+          {" "}
+          <VideoLoader />
+          <VideoLoader />
+          <VideoLoader />
+          <VideoLoader />
+          <VideoLoader />
+          <VideoLoader />
+          <VideoLoader />
         </>
-    );
+      );
+    if (!isLoading && isError && data?.length === 0)
+      content = <Error message='there was an server error' />;
+    if (!isLoading && !isError && data?.length > 0) {
+      content = data?.map((video) => <Video key={video.id} video={video} />);
+    }
+    return content;
+  }
+  /** <!-- content render --> */
+
+  return <>{contentRender()}</>;
 }
